7979122fd822c11bf923aa7dec8c244ef1f2e55c
Rot <formacio@BARCELO-EPMU2RV.formacio.local>
2024-07-03 13:36:55 +0200

    hello word controller

---
+++ sprint5/src/main/java/com/sprint5T1N2/rot/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.sprint5T1N2.rot.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public Mono<String> hello() {
+        return Mono.just("Hello, World!");    //prints Hello, World!
+    }
+}
